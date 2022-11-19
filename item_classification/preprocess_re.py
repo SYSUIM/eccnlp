300f9dbac8235898fc9118ba5851405281a97185
zyx92722 <114485820+zyx92722@users.noreply.github.com>
2022-11-19 21:01:21 +0800

    add logging and argparse (#5)
    * add the logging
    
    * add logging and argparse
    
    * add parameter and delete unused package
    
    Co-authored-by: panzy25 <pzy000301@gmail.com>

--- item_classification/preprocess_re.py
+++ item_classification/preprocess_re.py
@@ -1,31 +1,42 @@
-import numpy as np
-import pandas as pd
-import csv
 import re
 import random
-import threading
+import logging
+import argparse
 
-def re_pattern():
-    Codesdf = pd.read_excel('/data/xf2022/Projects/TextClassificationModel/data/raw_data/第一期手工标注工作核查结果-1113.xlsx')
+import numpy as np
+import pandas as pd
+
+
+parser = argparse.ArgumentParser(description='Data Processing')
+parser.add_argument('--data', type=str, required=True, help='path of raw data')
+parser.add_argument('--length', default=5, type=str, help='minimum length of clause')
+parser.add_argument('--type', default='业绩归因', type=str, help='type of answer')
+parser.add_argument('--log', type=str, required=True, help='path of log file')
+args = parser.parse_args()
+
+def re_pattern(args):
+    Codesdf = pd.read_excel(args.data)
 
     #仅保留前三类问题以及归因在问题中的样本
     Codesdf['Predict_非业绩归因'] = 0
     Codesdf.loc[(Codesdf['业绩归因问题类型'] != "业绩归因") & (Codesdf['业绩归因问题类型'] !=
                                                "回答特定因素对业绩是否有影响") & (Codesdf['业绩归因问题类型'] != "未来发展趋势"), '是否为业绩归因回答'] = 0
     Codesdf.loc[(Codesdf['原因归属在回答文本中'] == 0) & (Codesdf['是否为业绩归因回答'] == 1), '是否为业绩归因回答'] = 0
-    print("查看业绩归因样本情况\n", Codesdf['是否为业绩归因回答'].value_counts(),"\n")
+    #print("查看业绩归因样本情况\n", Codesdf['是否为业绩归因回答'].value_counts(),"\n")
+    logging.basicConfig(filename=args.log, level=logging.INFO, format='%(asctime)s %(message)s')
+    logging.info(("查看业绩归因样本情况",Codesdf['是否为业绩归因回答'].value_counts()))
+
 
     # 定义函数方便查看筛选效果
     def Prediction(Condition):
-        Length=len(Codesdf[(Codesdf['是否为业绩归因回答'] == 0)]  )
+        Length=len(Codesdf[(Codesdf['是否为业绩归因回答'] == 0)])
         Len1 = len(Codesdf[Codesdf['Predict_非业绩归因'] == 1])
         Len2 = len(Codesdf[(Codesdf['Predict_非业绩归因'] == 1)
                & (Codesdf['是否为业绩归因回答'] == 1)])
-        print("总非业绩归因条目数", Length)
-        print("增加%s后过滤数量:%d" %(Condition, Len1))
-        print("其中错误过滤数量", Len2)
-        print("其中正确过滤数量", Len1-Len2)
-        print("\n")
+        logging.info(("总非业绩归因条目数", Length))
+        logging.info(("增加%s后过滤数量:%d" %(Condition, Len1)))
+        logging.info(("其中错误过滤数量", Len2))
+        logging.info(("其中正确过滤数量", Len1-Len2))
 
     # 定义业绩归因表达式
     Attr_pattern = '业绩|利润|亏损|损失|收入|绩效|竞争力|成本|盈利|影响|利于|发展|竞争力'  # 放宽标准则仅包括'影响|利于|发展|竞争力'
@@ -60,12 +71,9 @@ def re_pattern():
 
     #输出结果
     Codesdf=Codesdf.drop(columns=['Length'])
-    Codesdf.to_csv('./data/raw_data/test.csv')
+    return Codesdf
 
-def get_dataset():
-    data = pd.read_csv('./data/raw_data/test.csv')
-    print(data.head())
-    data.info()
+def get_dataset(data, args):
     sentence_dict = {}
     sentence_set = set()
     for index, row in data.iterrows():
@@ -73,31 +81,24 @@ def get_dataset():
         if row['Predict_非业绩归因'] == 0:
             Acntet_cut = row['Acntet'].split('。')
             for sentence in Acntet_cut:
-                # 去除文本中各种字符，只保留汉字
-                sentence = re.sub('[^\u4e00-\u9fa5]','', str(sentence))
                 #设置切分后最小长度
-                if len(sentence) > 5:
+                if len(sentence) > args.length:
                 # if len(sentence) > 0:
                     #设置问题类型
-                    if row['业绩归因问题类型'] == '业绩归因':
+                    if row['业绩归因问题类型'] == args.type:
                     #if row['业绩归因问题类型'] == '业绩归因' or row['业绩归因问题类型'] == '回答特定因素对业绩是否有影响' or row['业绩归因问题类型'] == '未来发展趋势':
                         for i in range(1,11):
                             if str(row['原因归属' + str(i)]) is None:
                                 continue
                             if str(row['原因归属' + str(i)]) in str(sentence):
+                                # 去除文本中各种字符，只保留汉字
+                                sentence = re.sub('[^\u4e00-\u9fa5]','', str(sentence))
                                 sentence_dict[sentence] = 1
                                 sentence_set.add(sentence)
             
                     if sentence not in sentence_set:
+                        sentence = re.sub('[^\u4e00-\u9fa5]','', str(sentence))
                         sentence_dict[sentence] = 0
-        
-    with open('./data/raw_data/acntet_cut_test.csv','w',encoding='utf-8', newline='') as f:
-        f_csv = csv.writer(f)
-        f_csv.writerow(['Acntet','是否为业绩归因回答'])
-        for k in sentence_dict.keys():
-            f_csv.writerow([k, sentence_dict[k]])
-    f.close()
-
 
     train_list = []
     val_list = []
@@ -132,33 +133,14 @@ def get_dataset():
         train_balance_list.append(train_list[j])
     random.shuffle(train_balance_list)
 
-    data_train = open('/data/xf2022/Projects/TextClassificationModel/data/dataset_test/train.txt','w')
-    data_test = open('/data/xf2022/Projects/TextClassificationModel/data/dataset_test/test.txt', 'w', encoding='utf8')
-    data_val = open('/data/xf2022/Projects/TextClassificationModel/data/dataset_test/validation.txt', 'w', encoding='utf8')
-    data_description = open('/data/xf2022/Projects/TextClassificationModel/data/dataset_test/description.txt', 'w', encoding='utf8')
+    data_train=pd.DataFrame(train_balance_list)
+    data_val=pd.DataFrame(val_list)
+    data_test=pd.DataFrame(test_list)
 
-    for sentence in train_balance_list:
-        data_train.write(sentence + '\n')
-    data_train.close()
-    print('训练集写入完成。')
+    logging.info('问题类型：'+ args.type)
+    logging.info('训练集数量：' + str(len(train_list)))
+    logging.info('验证集数量：' + str(len(val_list)))
+    logging.info('测试集数量：' + str(len(test_list)))
+    logging.info('训练集均衡后数量：' + str(len(train_balance_list)))
 
-    for sentence in val_list:
-        data_val.write(sentence + '\n')
-    data_val.close()
-    print('验证集写入完成。')
-
-    for sentence in test_list:
-        data_test.write(sentence + '\n')
-    data_test.close()
-    print('测试集写入完成。')
-
-    data_description.write('切分不设阈值'+'\n'+'问题类型：业绩归因+回答特定因素对业绩是否有影响+未来发展趋势'+'\n')
-    data_description.write('训练集数量：' + str(len(train_list)) +'\n')
-    data_description.write('验证集数量：' + str(len(val_list)) +'\n')
-    data_description.write('测试集数量：' + str(len(test_list)) + '\n')
-    data_description.write('训练集均衡后数量：' + str(len(train_balance_list)) +'\n')
-    data_description.close()
-    
-if __name__ == '__main__':
-    re_pattern()
-    get_dataset()
+    return sentence_dict, data_train, data_val, data_test
