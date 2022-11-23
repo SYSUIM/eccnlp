1a0a4bb2c4952ba7b240e60c48e07cda8d7a983b
qiubai666 <3203550679@qq.com>
2022-11-23 22:26:45 +0800

    add the logging

--- item_classification/train_eval.py
+++ item_classification/train_eval.py
@@ -6,7 +6,8 @@ import torch.nn.functional as F
 from sklearn import metrics
 import time
 from utils import get_time_dif
-from tensorboardX import SummaryWriter
+import logging
+
 
 
 # 权重初始化，默认xavier
@@ -37,9 +38,8 @@ def train(config, model, train_iter, dev_iter, test_iter):
     dev_best_loss = float('inf')
     last_improve = 0  # 记录上次验证集loss下降的batch数
     flag = False  # 记录是否很久没有效果提升
-    writer = SummaryWriter(log_dir=config.log_path + '/' + time.strftime('%m-%d_%H.%M', time.localtime()))
     for epoch in range(config.num_epochs):
-        print('Epoch [{}/{}]'.format(epoch + 1, config.num_epochs))
+        logging.info('Epoch [{}/{}]'.format(epoch + 1, config.num_epochs))
         # scheduler.step() # 学习率衰减
         for i, (trains, labels) in enumerate(train_iter):
             outputs = model(trains)
@@ -62,21 +62,16 @@ def train(config, model, train_iter, dev_iter, test_iter):
                     improve = ''
                 time_dif = get_time_dif(start_time)
                 msg = 'Iter: {0:>6},  Train Loss: {1:>5.2},  Train Acc: {2:>6.2%},  Val Loss: {3:>5.2},  Val Acc: {4:>6.2%},  Time: {5} {6}'
-                print(msg.format(total_batch, loss.item(), train_acc, dev_loss, dev_acc, time_dif, improve))
-                writer.add_scalar("loss/train", loss.item(), total_batch)
-                writer.add_scalar("loss/dev", dev_loss, total_batch)
-                writer.add_scalar("acc/train", train_acc, total_batch)
-                writer.add_scalar("acc/dev", dev_acc, total_batch)
+                logging.info(msg.format(total_batch, loss.item(), train_acc, dev_loss, dev_acc, time_dif, improve))
                 model.train()
             total_batch += 1
             if total_batch - last_improve > config.require_improvement:
                 # 验证集loss超过1000batch没下降，结束训练
-                print("No optimization for a long time, auto-stopping...")
+                logging.info("No optimization for a long time, auto-stopping...")
                 flag = True
                 break
         if flag:
             break
-    writer.close()
     test(config, model, test_iter)
 
 
@@ -87,13 +82,13 @@ def test(config, model, test_iter):
     start_time = time.time()
     test_acc, test_loss, test_report, test_confusion = evaluate(config, model, test_iter, test=True)
     msg = 'Test Loss: {0:>5.2},  Test Acc: {1:>6.2%}'
-    print(msg.format(test_loss, test_acc))
-    print("Precision, Recall and F1-Score...")
-    print(test_report)
-    print("Confusion Matrix...")
-    print(test_confusion)
+    logging.info(msg.format(test_loss, test_acc))
+    logging.info("Precision, Recall and F1-Score...")
+    logging.info(test_report)
+    logging.info("Confusion Matrix...")
+    logging.info(test_confusion)
     time_dif = get_time_dif(start_time)
-    print("Time usage:", time_dif)
+    logging.info("Time usage:{}".format(time_dif))
 
 
 def evaluate(config, model, data_iter, test=False):
