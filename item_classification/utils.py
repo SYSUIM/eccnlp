92468828354eeba9f270c8043e71dcaac768ec5d
qiubai666 <3203550679@qq.com>
2022-11-23 22:25:17 +0800

    modify the data parameters

--- item_classification/utils.py
+++ item_classification/utils.py
@@ -6,25 +6,22 @@ import pickle as pkl
 from tqdm import tqdm
 import time
 from datetime import timedelta
+import logging
 
 
 MAX_VOCAB_SIZE = 30000  # 词表长度限制
 UNK, PAD = '<UNK>', '<PAD>'  # 未知字，padding符号
 
-
-def build_vocab(file_path, tokenizer, max_size, min_freq):
+# 传入dataframe格式
+def build_vocab(data_df, tokenizer, max_size, min_freq):
     vocab_dic = {}
-    with open(file_path, 'r', encoding='UTF-8') as f:
-        for line in tqdm(f):
-            lin = line.strip()
-            if not lin:
-                continue
-            content = lin.split('\t')[0]
-            for word in tokenizer(content):
-                vocab_dic[word] = vocab_dic.get(word, 0) + 1
-        vocab_list = sorted([_ for _ in vocab_dic.items() if _[1] >= min_freq], key=lambda x: x[1], reverse=True)[:max_size]
-        vocab_dic = {word_count[0]: idx for idx, word_count in enumerate(vocab_list)}
-        vocab_dic.update({UNK: len(vocab_dic), PAD: len(vocab_dic) + 1})
+    for i in tqdm(range(len(data_df))):
+        content = data_df.iloc[i, 0]
+        for word in tokenizer(content):
+            vocab_dic[word] = vocab_dic.get(word, 0) + 1
+    vocab_list = sorted([_ for _ in vocab_dic.items() if _[1] >= min_freq], key=lambda x: x[1], reverse=True)[:max_size]
+    vocab_dic = {word_count[0]: idx for idx, word_count in enumerate(vocab_list)}
+    vocab_dic.update({UNK: len(vocab_dic), PAD: len(vocab_dic) + 1})
     return vocab_dic
 
 
@@ -33,38 +30,31 @@ def build_dataset(config, ues_word):
         tokenizer = lambda x: x.split(' ')  # 以空格隔开，word-level
     else:
         tokenizer = lambda x: [y for y in x]  # char-level
-    if os.path.exists(config.vocab_path):
-        vocab = pkl.load(open(config.vocab_path, 'rb'))
-    else:
-        vocab = build_vocab(config.train_path, tokenizer=tokenizer, max_size=MAX_VOCAB_SIZE, min_freq=1)
-        pkl.dump(vocab, open(config.vocab_path, 'wb'))
-    print(f"Vocab size: {len(vocab)}")
+    vocab = build_vocab(config.train_df, tokenizer=tokenizer, max_size=MAX_VOCAB_SIZE, min_freq=1)
+    logging.info(f"Vocab size: {len(vocab)}")
 
-    def load_dataset(path, pad_size=32):
+    # 传入dataframe格式
+    def load_dataset(data_df, pad_size=32):
         contents = []
-        with open(path, 'r', encoding='UTF-8') as f:
-            for line in tqdm(f):
-                lin = line.strip()
-                if not lin:
-                    continue
-                content, label = lin.split('\t')
-                words_line = []
-                token = tokenizer(content)
-                seq_len = len(token)
-                if pad_size:
-                    if len(token) < pad_size:
-                        token.extend([PAD] * (pad_size - len(token)))
-                    else:
-                        token = token[:pad_size]
-                        seq_len = pad_size
-                # word to id
-                for word in token:
-                    words_line.append(vocab.get(word, vocab.get(UNK)))
-                contents.append((words_line, int(label), seq_len))
+        for i in tqdm(range(len(data_df))):
+            content, label = data_df.iloc[i, 0:2]
+            words_line = []
+            token = tokenizer(content)
+            seq_len = len(token)
+            if pad_size:
+                if len(token) < pad_size:
+                    token.extend([PAD] * (pad_size - len(token)))
+                else:
+                    token = token[:pad_size]
+                    seq_len = pad_size
+            # word to id
+            for word in token:
+                words_line.append(vocab.get(word, vocab.get(UNK)))
+            contents.append((words_line, int(label), seq_len))
         return contents  # [([...], 0), ([...], 1), ...]
-    train = load_dataset(config.train_path, config.pad_size)
-    dev = load_dataset(config.dev_path, config.pad_size)
-    test = load_dataset(config.test_path, config.pad_size)
+    train = load_dataset(config.train_df, config.pad_size)
+    dev = load_dataset(config.dev_df, config.pad_size)
+    test = load_dataset(config.test_df, config.pad_size)
     return vocab, train, dev, test
 
 
@@ -124,32 +114,23 @@ def get_time_dif(start_time):
     time_dif = end_time - start_time
     return timedelta(seconds=int(round(time_dif)))
 
-# 后续把main方法包装成函数
-if __name__ == "__main__":
-    '''提取预训练词向量'''
-    # 下面的目录、文件名按需更改。
-    train_dir = "./data/dataset/train.txt"
-    vocab_dir = "./data/dataset/vocab.pkl"
-    pretrain_dir = "./data/sgns.financial.char"
-    emb_dim = 300
-    filename_trimmed_dir = "./data/dataset/embedding_answer"
-    if os.path.exists(vocab_dir):
-        word_to_id = pkl.load(open(vocab_dir, 'rb'))
-    else:
-        # tokenizer = lambda x: x.split(' ')  # 以词为单位构建词表(数据集中词之间以空格隔开)
-        tokenizer = lambda x: [y for y in x]  # 以字为单位构建词表
-        word_to_id = build_vocab(train_dir, tokenizer=tokenizer, max_size=MAX_VOCAB_SIZE, min_freq=1)
-        pkl.dump(word_to_id, open(vocab_dir, 'wb'))
 
-    embeddings = np.random.rand(len(word_to_id), emb_dim)
-    f = open(pretrain_dir, "r", encoding='UTF-8')
-    for i, line in enumerate(f.readlines()):
-        # if i == 0:  # 若第一行是标题，则跳过
-        #     continue
-        lin = line.strip().split(" ")
-        if lin[0] in word_to_id:
-            idx = word_to_id[lin[0]]
-            emb = [float(x) for x in lin[1:301]]
-            embeddings[idx] = np.asarray(emb, dtype='float32')
-    f.close()
-    np.savez_compressed(filename_trimmed_dir, embeddings=embeddings)
+def get_utils(train_df):
+    '''提取预训练词向量'''
+    pretrain_dir = "../data/sgns.financial.char"
+    emb_dim = 300
+    # tokenizer = lambda x: x.split(' ')  # 以词为单位构建词表(数据集中词之间以空格隔开)
+    tokenizer = lambda x: [y for y in x]  # 以字为单位构建词表
+    word_to_id = build_vocab(train_df, tokenizer=tokenizer, max_size=MAX_VOCAB_SIZE, min_freq=1)
+
+    embeddings_pretrained = np.random.rand(len(word_to_id), emb_dim)
+    with open(pretrain_dir, "r", encoding='UTF-8') as f:
+        for i, line in enumerate(f.readlines()):
+            # if i == 0:  # 若第一行是标题，则跳过
+            #     continue
+            lin = line.strip().split(" ")
+            if lin[0] in word_to_id:
+                idx = word_to_id[lin[0]]
+                emb = [float(x) for x in lin[1:301]]
+                embeddings_pretrained[idx] = np.asarray(emb, dtype='float32')
+    return embeddings_pretrained
