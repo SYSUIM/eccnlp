f051e5179491f008764ef182eae3d99775a02b01
panzy25 <pzy000301@gmail.com>
2022-11-29 02:55:25 +0800

    modify

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from data_process.dataprocess import re_pattern, train_dataset, predict_dataset, classification_dataset
-from item_classification.run_classification_model import run_classification_model
 import argparse
 
+from data_process.dataprocess import re_pattern, train_dataset, predict_dataset, classification_dataset
+from item_classification.run_classification_model import run_classification_model
+
+
 parser = argparse.ArgumentParser(description='Chinese Text Classification')
+
+'''
+here are parameters for Chinese Text Classification
+'''
 parser.add_argument('--data', type=str, required=True, help='path of raw data')
 parser.add_argument('--min_length', default=5, type=str, help='not less than 0')
 parser.add_argument('--type', default='业绩归因', type=str, help='type of answer')
@@ -17,9 +23,14 @@ parser.add_argument('--n_vocab', default=0, type=int, help='Size of the vocab.')
 parser.add_argument('--batch_size', default=128, type=int, help='Batch size per GPU/CPU for training.')
 parser.add_argument('--pad_size', default=32, type=int, help='Size of the pad.')
 parser.add_argument('--learning_rate', default=1e-3, type=float, help='The initial learning rate for Adam.')
+
+'''
+here are parameters for Information Extraction
+'''
+
 args = parser.parse_args()
 
-if __name__ == '__main__':
+def text_classification(args):
     data = re_pattern(args)
     dataset = train_dataset(data, args)
     train_list, dev_list, test_list, train_dict, val_dict, test_dict = classification_dataset(dataset, args)
@@ -31,3 +42,6 @@ if __name__ == '__main__':
     with open("./data/result_data/result_data.txt", 'w', encoding='utf8') as f:
         for i in range(len(all_dict)):
             f.write(str(all_dict[i]) + '\n')
+
+if __name__ == '__main__':
+    text_classification(args)
