e357baa6426011a2b7d4ad6399fb783bc0577275
wdmmxlbt <2480860197@qq.com>
2023-01-15 23:50:55 +0800

    correct logging at line 79

--- phrase_rerank/test.py
+++ phrase_rerank/test.py
@@ -32,7 +32,7 @@ def test_model(args, test_list, logpath):
     test_data, test_data_keys = parse_labeled_data_file(args, test_list)
     test_data_key_count = len(test_data_keys)
     reason_list_test = reason_list
-    reason_id = int(math.floor(len(reason_list_test)/args.reason_num*0.7)*args.reason_num)
+    reason_id = 0
     saver = tf.train.Saver()
     all_reason_list=[]
 
@@ -76,8 +76,8 @@ def test_model(args, test_list, logpath):
             for i in range(o1.shape[0]):
                 log.info("%s\t%.2f\t%.2f\t%s\t%s" % (result_label, o[i][0], O[i], qid, o[i][1]))
         
-        log.info ("-- rank  top1 precision [%d/%d = %f] -- " % (
-                total_queries_count - falsepositive_rank_count,
+        log.info ("-- rank  %s precision [%d/%d = %f] -- " % (
+                args.top, total_queries_count - falsepositive_rank_count,
                 total_queries_count,
                 1.0 - 1.0 * falsepositive_rank_count / total_queries_count
         ))
@@ -87,20 +87,20 @@ def test_model(args, test_list, logpath):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='predict')
-    parser.add_argument('--MODEL_PATH', type=str, default='./data/data_model/model_v16_lambdarank.ckpt',help='模型保存路径')
+    parser.add_argument('--MODEL_PATH', type=str, default='./data/data_model/model_v19_lambdarank.ckpt',help='模型保存路径')
     parser.add_argument('--f_num', type=int, default=2, help='feature number')
     parser.add_argument('--type', type=str, default='业绩归因',help='type of answer')
     parser.add_argument('--reason_num', type=int, default=10,help='reason number')
-    parser.add_argument('--path_of_merged_reasons', type=str, default='/data/fkj2023/Project/eccnlp_local/phrase_rerank/data/res_log/2.0_2022-12-23_merge.txt',help='path of merged reasons')
+    parser.add_argument('--path_of_merged_reasons', type=str, default='/data/fkj2023/Project/eccnlp_local/phrase_rerank/data/res_log/2.0_2023-01-15_merge.txt',help='path of merged reasons')
     # parser.add_argument('--top', type=str, default='top1', help='choose top1 or top2')
     parser.add_argument('--top', type=str, required=True, help='choose top1 or top2')
     args = parser.parse_args()
 
     logpath = "/data/fkj2023/Project/eccnlp_local/phrase_rerank/data/res_log/"
     if(args.top == "top1"):
-        log = get_logger2('top1-M16',logpath)
+        log = get_logger2('top1-M19',logpath)
     if(args.top == "top2"):
-        log = get_logger2('top2-M16',logpath)
+        log = get_logger2('top2-M19',logpath)
     
     all_list, train_list, test_list, reason_list = form_input_list(args)
     test_model(args, test_list, logpath)
