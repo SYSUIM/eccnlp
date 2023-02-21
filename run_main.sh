2d3500404ae339552b9dc606042fcc25061e847b
panzy25 <pzy000301@gmail.com>
2023-02-21 11:52:10 +0800

    change to abstract path

--- run_main.sh
+++ run_main.sh
@@ -1,6 +1,6 @@
 nohup python -u main.py \
-    --data ./data/raw_data/第一第二期手工标注数据汇总-20221214.xlsx \
+    --data /data/xf2022/Projects/eccnlp_local/data/raw_data/第一第二期手工标注数据汇总-20221214.xlsx \
     --balance down \
     --model EnsembleModel \
     --ensemble_models TextCNN TextRNN TextRCNN TextRNN_Att DPCNN \
-    --num_epochs 20
+    --num_epochs 20 > $(date +%Y%m%d).log 2>&1 &
