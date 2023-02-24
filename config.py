e04089381d5ec1c61fb43a9c1fddf26f0b688f9d
zyx92722 <zhouyx73@mail2.sysu.edu.cn>
2023-02-24 17:05:20 +0800

    add re

--- config.py
+++ config.py
@@ -104,48 +104,49 @@ def get_arguments():
 
 def re_filter(dataset) -> list:
     for i in range(len(dataset)):
-        dataset[i].append(0)   # predict业绩归因 [-2]
-        if dataset[i][7] & dataset[i][9] ==1:
-            dataset[i][-1]=0                
-        dataset[i].append(len(dataset[i][6])) #length [-1]
+        dataset[i]['predict_非业绩归因'] = 0  # predict业绩归因
+        if dataset[i]['in_Acntet']==0 & dataset[i]['label'] ==1:
+            dataset[i]['predict_非业绩归因']=0                
+        dataset[i]['length']=len(dataset[i]['raw_text']) #length 
 
          # 定义业绩归因表达式
         Attr_pattern = '业绩|利润|亏损|损失|收入|绩效|竞争力|成本|盈利|影响|利于|发展|竞争力'  # 放宽标准则仅包括'影响|利于|发展|竞争力'
         
         # 拒不回答
-        if re.search('感谢|公告|报告|披露|回复|回答',dataset[i][6]): 
-            if (dataset[i][-1] <=30):
-                dataset[i][-2] = 1
-        if dataset[i][-1] < 20:
-            if re.match(Attr_pattern,dataset[i][6]):
-                dataset[i][-2] = 1
+        if re.search('感谢|公告|报告|披露|回复|回答',dataset[i]['raw_text']): 
+            if (dataset[i]['length'] <=30):
+                dataset[i]['predict_非业绩归因'] = 1
+        if dataset[i]['length'] < 20:
+            if re.match(Attr_pattern,dataset[i]['raw_text']):
+                dataset[i]['predict_非业绩归因'] = 1
         
         # 无关问题：股市、薪酬、信息披露
         pattern1 = '分红|派息|股权|利润分配|股份|股价|股市|市胆率|PE|不良率|大盘'
-        if re.match(pattern1,dataset[i][6]):
-            if not re.match(Attr_pattern,dataset[i][6]):
-                dataset[i][-2] = 1
-        if re.search('薪酬|信息披露',dataset[i][6]):
-            dataset[i][-2] = 1
+        if re.match(pattern1,dataset[i]['raw_text']):
+            if not re.match(Attr_pattern,dataset[i]['raw_text']):
+                dataset[i]['predict_非业绩归因'] = 1
+        if re.search('薪酬|信息披露',dataset[i]['raw_text']):
+            dataset[i]['predict_非业绩归因'] = 1
 
         # 答非所问
         pattern3 = '不*回答问题|回避问题|官话|答非所问|没有回[复答]|态度问题'
-        if re.match(pattern3,dataset[i][6]):
-            if not re.match(Attr_pattern,dataset[i][6]):
-                dataset[i][-2] = 1
+        if re.match(pattern3,dataset[i]['raw_text']):
+            if not re.match(Attr_pattern,dataset[i]['raw_text']):
+                dataset[i]['predict_非业绩归因'] = 1
 
         # 未来计划
         pattern4= "未来|计划|将|预计|下一年"
-        if re.match(pattern4,dataset[i][6]):
-            if not re.match(Attr_pattern,dataset[i][6]):
-                dataset[i][-2] = 1
+        if re.match(pattern4,dataset[i]['raw_text']):
+            if not re.match(Attr_pattern,dataset[i]['raw_text']):
+                dataset[i]['predict_非业绩归因'] = 1
                 
         # 暂无回复
         pattern5= "(没有|暂无)[^\r\n\t\f\v，,。？！]*影响"
-        if re.match(pattern5,dataset[i][6]):
-            dataset[i][-2] = 1
-            
-        dataset[i].pop()
+        if re.match(pattern5,dataset[i]['raw_text']):
+            dataset[i]['predict_非业绩归因'] = 1
+
+        dataset[i].pop('in_Acntet')
+        dataset[i].pop('length')
     return dataset
 
 if __name__ == '__main__':
