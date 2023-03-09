4fab84b3a311ecf20a606e90b65c7da3812fda59
wdmmxlbt <2480860197@qq.com>
2023-03-09 17:29:29 +0800

    update multiprocessing

--- run_main.sh
+++ run_main.sh
@@ -1,6 +1,11 @@
 nohup python -u main.py \
-    --data /data/zyx2022/FinanceText/process_file/2.1_raw_dataset_dict.txt \
+    --data /data/fkj2023/Project/eccnlp_local/data/2.1_raw_dataset_dict_test.txt \
     --balance down \
     --model EnsembleModel \
     --ensemble_models TextCNN TextRNN TextRCNN TextRNN_Att DPCNN \
-    --num_epochs 20 >>./log/$(date +%Y%m%d).log 2>&1 &
+    --num_epochs 10 \
+    --batch_size 64 \
+    --UIE_batch_size 4 \
+    --UIE_num_epochs 2 \
+    --device gpu:2 \
+    >>./log/$(date +%Y%m%d).log 2>&1 &
