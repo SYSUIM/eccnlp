9c174a4f92c6a032a2ee4071aee54e06564df0a3
wdmmxlbt <2480860197@qq.com>
2023-03-14 07:53:11 +0800

    update rerank_predict and print filtedDataset

--- inference/inference.py
+++ inference/inference.py
@@ -9,11 +9,11 @@ else:
 
 import sys
 # sys.path.append('/data/pzy2022/project/eccnlp')
-
 sys.path.append('/data/fkj2023/Project/eccnlp')
+# sys.path.append('/data/fkj2023/Project/eccnlp/inference')
 # sys.path.append('../')
 from config import re_filter
-from utils import read_list_file, split_dataset, evaluate_sentence
+from utils import read_list_file, split_dataset, evaluate_sentence, read_word, print_list, check_log_dir, get_res_logger
 from data_process.dataprocess import train_dataset, split_dataset, classification_dataset, dict_to_list
 
 import logging
@@ -26,7 +26,7 @@ from info_extraction.inference import extraction_inference
 
 # from data_process.info_extraction import dataset_generate_train
 
-from phrase_rerank.rank_data_process import get_logger1,  form_predict_input_list, add_embedding, get_text_list, merge_reasons,read_word, print_list
+from phrase_rerank.rank_data_process import get_logger1,  form_predict_input_list, add_embedding, get_text_list, merge_reasons
 from phrase_rerank.lambdarank import LambdaRank, add_rerank, predict
 import numpy as np
 
@@ -60,32 +60,28 @@ def extraction(args, dataset):
     _, _, result = extraction_inference(args, None, None, dataset)
     return result
 
-def rerank_predict(args, uie_list):
-    logpath4 = "/data/fkj2023/Project/eccnlp_local/phrase_rerank/data/inference/" 
-    log1 = get_logger1('3.1_embedding',logpath4)
-    log2 = get_logger1('3.1_merge',logpath4)
-    log3 = get_logger1('3.1_inference_add_rerank',logpath4)
+def rerank_predict(args, uie_list, log):
 
     word = read_word(args.word_path)
     after_embedding_list = add_embedding(args, uie_list)
-    '''
-    TODO change log.
-    '''
-    print_list(after_embedding_list, log1)
     text_list, num_list = get_text_list(uie_list)
     merged_list = merge_reasons(args, text_list, num_list, after_embedding_list)
-    print_list(merged_list, log2)
     predict_list, reasons = form_predict_input_list(args, merged_list, word)
     predict_data = np.array(predict_list)
     predicted_list, rerank_reasons, rerank_scores = predict(args, predict_data, reasons)
-    res = add_rerank(args, rerank_reasons,rerank_scores, merged_list, log3)
+    res = add_rerank(args, rerank_reasons,rerank_scores, merged_list, log)
     return res
 
 
 
 if __name__ == '__main__':
     args = config.get_arguments()
-    
+
+    log_path = check_log_dir(args.time) 
+    # print(log_path)
+    # exit(0)
+
+    inference_logger = get_res_logger('inference_logger', log_path + '/inference.log')    
     # read predict data...
     # predict_dataset = read_list_file(args.predict_data)
     # logging.info(f'length of raw dataset: {len(predict_dataset)}')
@@ -107,7 +103,9 @@ if __name__ == '__main__':
     # dataset = re_filter(dataset)
 
     filtedDataset = bertFilter(args, dataset)
-
+    filtedDataset_logger = get_res_logger('filtedDataset', log_path + '/filtedDataset.log')
+    print_list(filtedDataset, filtedDataset_logger)
+    
     # train_data, dev_data, test_data = dataset_generate_train(args, dataset)
     # logging.info(f'{len(dataset)} samples left after re_filter')
     
@@ -122,6 +120,6 @@ if __name__ == '__main__':
     # filepath = '/data/fkj2023/Project/eccnlp_local/phrase_rerank/info_extraction_result_1222.txt'
     # uie_list = read_list_file(filepath)
     # rerank_res = rerank_predict(args, uie_list)
-    rerank_res = rerank_predict(args, result)
+    rerank_res = rerank_predict(args, result, inference_logger)
 
 
