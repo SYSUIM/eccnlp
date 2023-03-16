15ab0a66faafb3f70051329e90e36c08f9fc51ba
wdmmxlbt <2480860197@qq.com>
2023-03-16 22:56:17 +0800

    output to txt

--- inference/inference.py
+++ inference/inference.py
@@ -2,13 +2,13 @@ import os
 import config
 
 import sys
-
+sys.path.append('/data/fkj2023/Project/eccnlp')
 from config import re_filter
 from utils import read_list_file, split_dataset, evaluate_sentence, read_word, print_list, check_log_dir, get_res_logger
 from data_process.dataprocess import train_dataset, split_dataset, classification_dataset, dict_to_list
 
 import logging
-sys.path.append('/data/fkj2023/Project/eccnlp')
+
 from item_classification.bert_inference import bertForSequenceClassification
 
 from item_classification.classification_model_predict import classification_models_predict
@@ -17,9 +17,10 @@ from info_extraction.inference import extraction_inference
 
 # from data_process.info_extraction import dataset_generate_train
 
-from phrase_rerank.rank_data_process import get_logger1,  form_predict_input_list, add_embedding, get_text_list, merge_reasons
-from phrase_rerank.lambdarank import LambdaRank, add_rerank, predict
+from phrase_rerank.rank_data_process import form_predict_input_list, add_embedding, get_text_list, merge_reasons, uie_list_filter
+from phrase_rerank.lambdarank import LambdaRank, add_rerank, predict_rank
 import numpy as np
+from datetime import datetime
 
 logging.basicConfig(
     level=logging.INFO,
@@ -51,16 +52,17 @@ def extraction(args, dataset):
     _, _, result = extraction_inference(args, None, None, dataset)
     return result
 
-def rerank_predict(args, uie_list, log):
+def rerank_predict(args, uie_list):
 
     word = read_word(args.word_path)
-    after_embedding_list = add_embedding(args, uie_list)
-    text_list, num_list = get_text_list(uie_list)
+    filtered_uie_list, context_list = uie_list_filter(args, uie_list)
+    after_embedding_list = add_embedding(args, filtered_uie_list)
+    text_list, num_list = get_text_list(filtered_uie_list)
     merged_list = merge_reasons(args, text_list, num_list, after_embedding_list)
     predict_list, reasons = form_predict_input_list(args, merged_list, word)
     predict_data = np.array(predict_list)
-    predicted_list, rerank_reasons, rerank_scores = predict(args, predict_data, reasons)
-    res = add_rerank(args, rerank_reasons,rerank_scores, merged_list, log)
+    predicted_list, rerank_reasons, rerank_scores = predict_rank(args, predict_data, reasons)
+    res = add_rerank(args, rerank_reasons, rerank_scores, merged_list)
     return res
 
 
@@ -71,8 +73,7 @@ if __name__ == '__main__':
     log_path = check_log_dir(args.time) 
     # print(log_path)
     # exit(0)
-
-    inference_logger = get_res_logger('inference_logger', log_path + '/inference.log')    
+ 
     # read predict data...
     # predict_dataset = read_list_file(args.predict_data)
     # logging.info(f'length of raw dataset: {len(predict_dataset)}')
@@ -94,9 +95,9 @@ if __name__ == '__main__':
     # dataset = re_filter(dataset)
 
     filtedDataset = bertFilter(args, dataset)
-    filtedDataset_logger = get_res_logger('filtedDataset', log_path + '/filtedDataset.log')
-    print_list(filtedDataset, filtedDataset_logger)
-    
+    with open("./log/" + datetime.now().strftime("%Y-%m-%d")+"_3.1_bertfiltedDataset.txt", 'w') as f:
+        [f.write(str(data) + '\n') for data in filtedDataset]    
+
     # train_data, dev_data, test_data = dataset_generate_train(args, dataset)
     # logging.info(f'{len(dataset)} samples left after re_filter')
     
@@ -111,6 +112,11 @@ if __name__ == '__main__':
     # filepath = '/data/fkj2023/Project/eccnlp_local/phrase_rerank/info_extraction_result_1222.txt'
     # uie_list = read_list_file(filepath)
     # rerank_res = rerank_predict(args, uie_list)
-    rerank_res = rerank_predict(args, result, inference_logger)
+    rerank_res = rerank_predict(args, result)
+
+    logging.info(f'length of rerank_res: {len(rerank_res)}')
+    
+    with open("./log/" + datetime.now().strftime("%Y-%m-%d")+"_3.1_res_add_rerank.txt", 'w') as f:
+        [f.write(str(data) + '\n') for data in rerank_res]    
 
 
