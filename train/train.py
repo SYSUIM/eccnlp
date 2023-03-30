28d3a629420b599611586fbdacbd2b1dd55d8c45
wdmmxlbt <2480860197@qq.com>
2023-03-30 21:00:41 +0800

    uncomment

--- train/train.py
+++ train/train.py
@@ -135,20 +135,20 @@ def run_information_extraction(args, data):
     train_data, dev_data, test_data = dataset_generate_train(args, data)
     logging.info(f'train_data: {len(train_data)}, dev_data: {len(dev_data)}, test_data: {len(test_data)}')
 
-    # do_train(device = args.device,
-    #          seed = args.seed,
-    #          model_dir = args.model_dir,
-    #          UIE_model = args.UIE_model,
-    #          UIE_batch_size = args.UIE_batch_size,
-    #          max_seq_len = args.max_seq_len,
-    #          init_from_ckpt = args.init_from_ckpt,
-    #          UIE_learning_rate = args.UIE_learning_rate,
-    #          UIE_num_epochs = args.UIE_num_epochs,
-    #          logging_steps = args.logging_steps,
-    #          valid_steps = args.valid_steps,
-    #          save_dir = args.save_dir,
-    #          train_data = train_data,
-    #          dev_data = dev_data)
+    do_train(device = args.device,
+             seed = args.seed,
+             model_dir = args.model_dir,
+             UIE_model = args.UIE_model,
+             UIE_batch_size = args.UIE_batch_size,
+             max_seq_len = args.max_seq_len,
+             init_from_ckpt = args.init_from_ckpt,
+             UIE_learning_rate = args.UIE_learning_rate,
+             UIE_num_epochs = args.UIE_num_epochs,
+             logging_steps = args.logging_steps,
+             valid_steps = args.valid_steps,
+             save_dir = args.save_dir,
+             train_data = train_data,
+             dev_data = dev_data)
     
     result_on_train_data, result_on_dev_data, result_on_test_data = extraction_inference(train_data, dev_data, test_data, args.type, args.save_dir, args.position_prob)
     
@@ -231,17 +231,17 @@ if __name__ == '__main__':
     #     print(i)
     # exit(0)
 
-    # report, matrix = BertForClassification(args, raw_dataset)
-    # print(report, matrix)
+    report, matrix = BertForClassification(args, raw_dataset)
+    print(report, matrix)
 
 
     result_on_train_data, result_on_dev_data, result_on_test_data = run_information_extraction(args, raw_dataset)
-    # uie_list = result_on_train_data + result_on_dev_data + result_on_test_data
+    uie_list = result_on_train_data + result_on_dev_data + result_on_test_data
 
     # run_rerank
-    # word = build_thesaurus(raw_dataset, args.t_path)
-    # run_rerank(args, uie_list, word)
-    # exit(0)
+    word = build_thesaurus(raw_dataset, args.t_path)
+    run_rerank(args, uie_list, word)
+    exit(0)
 
     # all_dict = text_classification(args)
     # all_dict = ensemble_text_classification(args, dataset)
